48f2eb372aea571f4ee2f4ad41e89fe55e4182c5
Indre Bakeer <indre.bakeer@gmail.com>
2018-12-31 05:33:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,6 +10,7 @@ def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
   neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
 end
 
+<<<<<<< HEAD
 great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
 
 def using_pop(great_hits_of_the_nineties)
@@ -25,6 +26,20 @@ end
 my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
 
 def using_shift(my_favorite_cities)
+=======
+def using_pop(great_hits_of_the_nineties)
+  great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+  great_hits_of_the_nineties == great_hits_of_the_nineties.pop
+end
+
+def pop_with_args(chars_in_game_of_thrones)
+  chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
+  chars_in_game_of_thrones.pop(2)
+end
+
+def using_shift(my_favorite_cities)
+  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+>>>>>>> c76041ab84d3190aa7afd242a9933939d5248a8b
   my_favorite_cities.shift
 end
 
@@ -33,10 +48,16 @@ def shift_with_args(ice_cream_brands)
   ice_cream_brands.shift(2)
 end
 
+<<<<<<< HEAD
   my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["mario kart", "flatiron school"]
 
 def using_concat(my_favorite_things, more_favs)
+=======
+def using_concat(my_favorite_things, more_favs)
+  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["mario kart", "flatiron school"]
+>>>>>>> c76041ab84d3190aa7afd242a9933939d5248a8b
   my_favorite_things.concat(more_favs)
 end
 
@@ -56,10 +77,16 @@ def using_flatten(private_colleges_in_newyork)
   private_colleges_in_newyork.flatten
 end
 
+<<<<<<< HEAD
 instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
 
 def using_delete(instructors, no_offense_steven = "Steven")
   instructors.delete(no_offense_steven)
+=======
+def using_delete(instructors, no_offense_steven)
+  instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+  no_offense_steven = instructors.delete("Steven")
+>>>>>>> c76041ab84d3190aa7afd242a9933939d5248a8b
 end
 
 def using_delete_at(famous_robots, deleted_robots)
@@ -68,6 +95,7 @@ def using_delete_at(famous_robots, deleted_robots)
 end
 
 
+<<<<<<< HEAD
 
 
 
@@ -82,3 +110,5 @@ end
 
 
 
+=======
+>>>>>>> c76041ab84d3190aa7afd242a9933939d5248a8b
